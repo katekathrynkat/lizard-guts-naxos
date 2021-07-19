8df6a70baa6924727e4196a3a6e94686de246232
Kate Culhane <kathryn.culhane@gmail.com>
2021-07-19 09:55:27 -0700

    update site

--- R_scripts/create_docs.R
+++ R_scripts/create_docs.R
@@ -11,17 +11,18 @@ library(katereR)
 ##### Create docs for website #####
 # Render to /docs
 
-rmarkdown::render_site("R_markdowns")
+rmarkdown::render_site("R_markdown")
 
 ##### Standalone HTML docs #####
 # Render to /reports
 
+#### DO NOT USE THIS CODE IT ERASES FILES?!?!?!?!
 # Index
 katereR::render_report(input = "../R_markdowns/index.Rmd",
                        output_file = "../reports/index.html")
 
 # 01 Metadata
-katereR::render_report(input = "../R_markdowns/01_metadata.html",
+katereR::render_report(input = "R_markdowns/01_metadata.html",
                        output_file = "../reports/01_metadata.html")
 
 # 02 Data wrangling
