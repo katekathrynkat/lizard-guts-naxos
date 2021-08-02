faa1a243f18cca8d7d48515d97610a3552a31260
Kate Culhane <kathryn.culhane@gmail.com>
2021-08-02 10:06:06 -0700

    update organization, data exploration

--- R_scripts/create_docs.R
+++ R_scripts/create_docs.R
@@ -16,15 +16,14 @@ rmarkdown::render_site("R_markdowns")
 ##### Standalone HTML docs #####
 # Render to /reports
 
-#### DO NOT USE THIS CODE IT ERASES FILES?!?!?!?!
 # Index
-# katereR::render_report(input = "../R_markdowns/index.Rmd",
-#                        output_file = "../reports/index.html")
-# 
-# # 01 Metadata
-# katereR::render_report(input = "R_markdowns/01_metadata.html",
-#                        output_file = "../reports/01_metadata.html")
-# 
-# # 02 Data wrangling
-# katereR::render_report(input = "R_markdowns/02_data_wrangling.html",
-#                        output_file = "../reports/02_data_wrangling.html")
+katereR::render_report(input = "R_markdowns/index.Rmd",
+                       output_file = "../reports/index.html")
+
+# 01 Metadata
+katereR::render_report(input = "R_markdowns/00_datasets.Rmd",
+                       output_file = "../reports/00_datasets.html")
+
+# 02 Data wrangling
+katereR::render_report(input = "R_markdowns/01_data_exploration.Rmd",
+                       output_file = "../reports/01_data_exploration.html")
