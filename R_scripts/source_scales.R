f748348f2c85cf42728a9d85826bc4a3eda105ea
Kate Culhane <kathryn.culhane@gmail.com>
2021-08-11 14:22:52 -0700

    data exploration

---
+++ R_scripts/source_scales.R
@@ -0,0 +1,16 @@
+##############################
+#####     Scales     #####
+##############################
+
+# Vectors for graph aesthetics
+
+##### Color scales #####
+
+# By trap type
+scol_trap <- c("red", "orange")
+names(scol_trap) <- c("pitfall", "sticky")
+
+
+# By sample type
+scol_sample <- c("blue", "red", "orange")
+names(scol_sample) <- c("diet", "pitfall", "sticky")
